6fef7a1d823d25580d3a381da7b3e6461971bad7
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-04-04 15:09:45 -0400

    Write views

--- gift/views.py
+++ gift/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Address, Item, Box
-from .serializers import AddressSerializer, BoxSerializer
+from .models import Address, Item, Box, User, Order
+from .serializers import AddressSerializer, BoxSerializer, ItemSerializer, UserSerializer, OrderSerializer
 # Create your views here.
 
 
@@ -23,3 +23,33 @@ class BoxList(generics.ListCreateAPIView):
 class BoxDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Box.objects.all()
     serializer_class = BoxSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class OrderList(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class ItemList(generics.ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
