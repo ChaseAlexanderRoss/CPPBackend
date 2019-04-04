5295ffb46af880d9749fd279bb8a10eab7d5cc0c
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-04-04 12:28:12 -0400

    Created models and superuser, activated server

--- gift/admin.py
+++ gift/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Address, Box, Item, Product, Card, Giftcard, Order, User
 # Register your models here.
+
+admin.site.register([Address, Box, Item, Product, Card, Giftcard, Order, User])
